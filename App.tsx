1c5bbabfbbda3f85b27757ccb438ecd59e7a45dc
GDPWinnerPranata <winner.pranata@gdpblabs.id>
2023-06-16 13:04:21 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,139 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import type { PropsWithChildren } from 'react';
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  Header
+} from 'react-native/Libraries/NewAppScreen';
+
+import DIDKit from '@spruceid/didkit-wasm-node';
+import { v4 } from 'uuid';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({ children, title }: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+async function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [vc, setVc] = useState<any>(null);
+
+  const key = DIDKit.generateEd25519Key();
+
+  const issuerId = DIDKit.keyToDID('key', key);
+  const verificationMethod = await DIDKit.keyToVerificationMethod('key', key);
+
+  const unsignedCredential = {
+    "@context": [
+      "https://www.w3.org/2018/credentials/v1",
+      "https://www.w3.org/2018/credentials/examples/v1"
+    ],
+    "id": v4(),
+    "type": [
+      "VerifiableCredential",
+      "IssuerCredential"
+    ],
+    "credentialSubject": {
+      "name": "Winner Pranata",
+      "title": "King of Scotland"
+    },
+    "issuer": issuerId,
+    "issuanceDate": new Date().toISOString()
+  }
+
+  const credentialOptions = {
+    "proofPurpose": "assertionMethod",
+    "verificationMethod": verificationMethod
+  }
+
+  setVc(await DIDKit.issueCredential(JSON.stringify(unsignedCredential), JSON.stringify(credentialOptions), key))
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="VC Demo">
+            {vc}
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
